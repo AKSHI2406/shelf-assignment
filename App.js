62a14747ed905e5a5f47905e73e519fd12698163
Akshita <akshitapandey2406@gmail.com>
2024-07-06 16:47:28 +0530

    removed spaces

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import { useFonts } from "expo-font";
 import TabNavigation from "./navigation/TabNavigation";
 import customFonts from "./utilities/constants/customFonts";
 
-
-
 export default function App() {
   const [isLoaded] = useFonts(customFonts);
   return <PaperProvider>{isLoaded ? <TabNavigation /> : <></>}</PaperProvider>;
